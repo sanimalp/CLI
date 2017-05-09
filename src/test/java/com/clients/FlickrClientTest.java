b6bc2a542d9bb31c8f471e1de57c547853ad3a2c
Jon Steege <sanimalp@gmail.com>
2017-05-09 01:16:22 -0600

    added test classes.

---
+++ src/test/java/com/clients/FlickrClientTest.java
@@ -0,0 +1,14 @@
+package com.clients;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FlickrClientTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/main/CLIMainTest.java
@@ -0,0 +1,14 @@
+package com.main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CLIMainTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
